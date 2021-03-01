7137fab95ebfc063906ae14bca3f427dc8a662b4
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-03-01 12:27:04 -1000

    set up controllers

---
+++ W7D1/skeleton/app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:session_token] = reset_session_token!
+      redirect_to user_url(@user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
