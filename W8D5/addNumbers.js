eca520d89587c2f161a98388b7754fa7f4db1411
Suiki and Timothy <teriley138@gmail.com>
2021-03-12 16:54:53 -0800

    do addNumber

---
+++ W8D5/addNumbers.js
@@ -0,0 +1,26 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const addNumbers = (sum = 0, numsLeft, completionCallback) => {
+  if (numsLeft > 0) {
+    reader.question("Enter a number: ", function (num) {
+      const number = parseInt(num);
+      sum += number;
+      console.log(sum);
+      addNumbers(sum, numsLeft - 1, completionCallback);
+    });
+  }
+
+  if (numsLeft === 0) {
+    completionCallback(sum);
+  }
+};
+
+addNumbers(0, 3, (sum) => {
+  console.log(`Total Sum: ${sum}`);
+  reader.close();
+});
