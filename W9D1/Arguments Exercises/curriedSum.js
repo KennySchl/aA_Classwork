24cbbcbcc1571d89d83a098a56ea49e4054eace9
Kenneth Schlappkohl <kschlappkohl@gmail.com>
2021-03-15 12:27:35 -1000

    complete prototypal inheritance

--- W9D1/Arguments Exercises/curriedSum.js
+++ W9D1/Arguments Exercises/curriedSum.js
@@ -1,14 +1,14 @@
 const curriedSum = function (numArgs) {
   let numbers = [];
-  return function _curriedSum (num) {
+  return function _curriedSum(num) {
     numbers.push(num);
     if (numbers.length === numArgs) {
-      return numbers.reduce((acc,el)=>acc+el);
+      return numbers.reduce((acc, el) => acc + el);
     } else {
       return _curriedSum;
     }
-  }
-}
+  };
+};
 
 // const sum = curriedSum(4);
 // console.log(sum(5)(30)(20)(1)); // => 56
@@ -16,26 +16,25 @@ const curriedSum = function (numArgs) {
 Function.prototype.curry = function (numArgs) {
   let that = this;
   let args = [];
-  return function _curry (arg) {
+  return function _curry(arg) {
     args.push(arg);
     if (args.length === numArgs) {
       return that.apply(null, args);
     } else {
       return _curry;
     }
-  }
-}
-
+  };
+};
 
 Function.prototype.curry2 = function (numArgs) {
   let that = this;
   let args = [];
-  return function _curry (arg) {
+  return function _curry(arg) {
     args.push(arg);
     if (args.length === numArgs) {
       return that(...args);
     } else {
       return _curry;
     }
-  }
-}
+  };
+};
